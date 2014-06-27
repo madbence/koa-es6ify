29d28017a1f0dc28e7c7b96dc7ba876e060079dc
Bence Dányi <bence@danyi.me>
2014-06-27 13:43:26 +0200

    Add middleware

---
+++ index.js
@@ -0,0 +1,34 @@
+var browserify = require('browserify');
+var es6ify = require('es6ify');
+
+module.exports = function middleware(opts) {
+  var opts = opts || {};
+  opts.add = opts.add || [];
+  opts.transform = opts.transform || [];
+  if(!opts.entry) {
+    throw new Error('You must specify browserify entry point! (`opts.entry`)');
+  }
+  it(!opts.path) {
+    throw new Error('You must specify `opts.path` to serve bundle!')
+  }
+  var b = browserify().add(es6ify.runtime);
+  opts.add.forEach(function(add) {
+    b.add(add);
+  });
+  opts.transform.forEach(function(transform) {
+    b.transform(transform);
+  });
+  b.transform(es6ify);
+  b.require(opts.entry, {
+    entry: true
+  });
+  return function* bundle(next) {
+    if(this.url != opts.path) {
+      return yield* next;
+    }
+    this.body = b.bundle({
+      debug: true
+    });
+    this.type = 'application/javascript'
+  };
+};
